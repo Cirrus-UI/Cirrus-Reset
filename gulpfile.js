635ea3915310e2a54de9834b6c215337e1253fa6
Stan <slim679975@gmail.com>
2017-05-07 12:08:39 -0400

    Fixed gulp script and corrected reset file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 'use strict'
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var prop = require('./package.json');
+var $ = require('gulp-load-plugins')();
+var head = '\/*\r\n* Cirrus Reset ' + prop.version + '\r\n* Stanley Lim, Copyright 2017\r\n* https://spiderpig86.github.io/Cirrus-Reset\r\n*/\r\n';
 
 gulp.task('sass', () => {
-    gulp.src('./sass/**/*.scss')
+    return gulp.src([
+        './sass/base.scss',
+        './sass/fix.scss'
+    ])
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist'));
+        .pipe($.concat('reset.css'))
+        .pipe($.header(head))
+        .pipe($.size())
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('sass:watch', () => {
