3fa5da52b4c7ec084bc6ddb44da942a38a8077cc
Stanley Lim <slim679975@gmail.com>
2021-09-05 00:57:09 -0700

    :sparkles: feat(gulpfile.js): Update Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-'use strict'
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var prop = require('./package.json');
-var minify = require('gulp-minify-css');
-var $ = require('gulp-load-plugins')();
-var head = '\/*\r\n* Cirrus Reset ' + prop.version + '\r\n* Stanley Lim, Copyright 2017\r\n* https://spiderpig86.github.io/Cirrus-Reset\r\n*/\r\n';
+'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prop = require('./package.json');
+const minify = require('gulp-minify-css');
+const $ = require('gulp-load-plugins')();
+const head =
+    '/*\r\n* Cirrus Reset ' +
+    prop.version +
+    `\r\n* Stanley Lim, Copyright ${new Date().getFullYear()}\r\n* https://github.com/Cirrus-UI/Cirrus-Reset\r\n*/\r\n`;
 
-gulp.task('sass', () => {
-    return gulp.src([
-        './sass/base.scss',
-        './sass/fix.scss'
-    ])
+gulp.task('compile', () => {
+    return gulp
+        .src(['./src/**/*.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe($.concat('reset.css'))
         .pipe($.header(head))
@@ -18,15 +19,49 @@ gulp.task('sass', () => {
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('sass:watch', () => {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('compile:watch', () => {
+    gulp.watch('./src/**/*.scss', ['sass']);
 });
 
-gulp.task('minify', ['sass'], () => {
-    return gulp.src(['./dist/reset.css'])
-        .pipe(minify())
-        .pipe($.header(head))
-        .pipe($.size())
-        .pipe($.concat('reset.min.css'))
-        .pipe(gulp.dest('./dist/'));
-});
+gulp.task(
+    'minify',
+    gulp.series('compile', () => {
+        return gulp
+            .src(['./dist/reset.css'])
+            .pipe(
+                minify(
+                    {
+                        level: {
+                            1: {
+                                all: true,
+                                normalizeUrls: false,
+                            },
+                            2: {
+                                all: false,
+                                removeDuplicateRules: true,
+                                reduceNonAdjacentRules: true,
+                                removeDuplicateFontRules: true,
+                                removeDuplicateMediaBlocks: true,
+                                mergeAdjacentRules: true,
+                                mergeIntoShorthands: true,
+                                mergeMedia: true,
+                                mergeNonAdjacentRules: true,
+                                mergeSemantically: false,
+                                removeEmpty: true,
+                            },
+                        },
+                    },
+                    (details) => {
+                        console.log(details.name + ': ' + details.stats.originalSize);
+                        console.log(details.name + '-min: ' + details.stats.minifiedSize);
+                    }
+                )
+            )
+            .pipe($.header(head))
+            .pipe($.size())
+            .pipe($.concat('reset.min.css'))
+            .pipe(gulp.dest('./dist/'));
+    })
+);
+
+gulp.task('default', gulp.series('minify'));
